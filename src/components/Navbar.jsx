8eae794e4833ecc89ef74b9143d27ad000065f22
Soorjyakanta Sethy <soorjyakant17@gmail.com>
2023-08-21 01:11:00 +0530

    change in names

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Soorjyakant &nbsp;
-            <span className='sm:block hidden'> | JavaScript Mastery</span>
+            <span className='sm:block hidden'> | Portfolio</span>
           </p>
         </Link>
 
